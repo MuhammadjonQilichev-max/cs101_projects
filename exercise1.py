d02f0237f73fae3dde54e141dcba1690d1bc4c60
Muhammadjon <muhammadqilichev081@gmail.com>
2025-10-07 11:44:09 +0500

    Add exercise1.py, a simple mad libs style program

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+celsius = float(input("enter the temperature in celsius"))
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15
+
+print(f'Temperature in Fahrenheit: {fahrenheit}')
+print(f'Temperature in Kelvin: {kelvin}')
