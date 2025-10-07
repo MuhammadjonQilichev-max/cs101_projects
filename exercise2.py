aaca7af94701ba81ece1f9ae16d0a1924d19324c
Muhammadjon <muhammadqilichev081@gmail.com>
2025-10-07 12:01:29 +0500

    Add exercise2.py, a simple mad libs style program

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+radius = float(input('Enter the radius of area:'))
+pi = 3.14159
+Area = pi * radius ** 2
+Circumference = 2 * pi * radius
+
+print(f'The area of the circle: {Area}')
+print(f'The circumference of the circle: {Circumference}')
